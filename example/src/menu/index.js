56d8a86a163addef23f1bea1a7921f2c1de7685f
ldd <leonardoldd@gmail.com>
2020-04-03 14:48:58 -0400

    refactor(example): extract responsive menu

---
+++ example/src/menu/index.js
@@ -0,0 +1,75 @@
+// prettier-ignore
+import {
+    Button, Menu, MenuLabel, MenuLink, MenuList,
+    Dropdown, DropdownContent, DropdownItem, DropdownMenu, DropdownTrigger
+  } from "bloomer";
+import React, { useState } from "react";
+
+export const MyMobileMenu = ({ clickHandler, labels = [] }) => {
+  const [dropdownStatus, setDropdownStatus] = useState(false);
+  return (
+    <Dropdown
+      style={{ marginBottom: "1.5rem" }}
+      isHidden={["desktop", "tablet"]}
+      isActive={dropdownStatus}
+    >
+      <DropdownTrigger>
+        <Button
+          isColor="light"
+          isOutlined
+          aria-haspopup="true"
+          aria-controls="dropdown-menu"
+          onClick={() => setDropdownStatus(!dropdownStatus)}
+        >
+          Example Picker &nbsp; ∨
+        </Button>
+      </DropdownTrigger>
+      <DropdownMenu>
+        <DropdownContent>
+          {labels.map(label => (
+            <DropdownItem key={label} onClick={() => clickHandler(label)}>
+              {label}
+            </DropdownItem>
+          ))}
+        </DropdownContent>
+      </DropdownMenu>
+    </Dropdown>
+  );
+};
+
+export const MenuEntry = ({ clickHandler, label, entries = [] }) => (
+  <>
+    <MenuLabel>{label}</MenuLabel>
+    <MenuList>
+      {entries.map(entry => (
+        <li key={entry}>
+          <MenuLink onClick={() => clickHandler(entry)}>{entry}</MenuLink>
+        </li>
+      ))}
+    </MenuList>
+  </>
+);
+
+export const MyDesktopMenu = ({ choices = [], clickHandler }) => (
+  <Menu style={{ paddingRight: "1.5rem" }} isHidden="mobile">
+    <MenuEntry
+      label={"Simple"}
+      entries={choices.slice(0, 1)}
+      clickHandler={clickHandler}
+    />
+    <MenuEntry
+      label={"Decorated Links"}
+      entries={choices.slice(1)}
+      clickHandler={clickHandler}
+    />
+  </Menu>
+);
+
+export const MyMenu = ({ clickHandler, choices = [] }) => (
+  <>
+    <MyDesktopMenu choices={choices} clickHandler={clickHandler} />
+    <MyMobileMenu labels={choices} clickHandler={clickHandler} />
+  </>
+);
+
+export default MyMenu;
--- example/src/page/Examples.js
+++ example/src/page/Examples.js
@@ -1,62 +1,20 @@
-// prettier-ignore
-import {
-  Button, Columns, Column, Menu, MenuLabel, MenuLink, MenuList,
-  Dropdown, DropdownContent, DropdownItem, DropdownMenu, DropdownTrigger
-} from "bloomer";
+import { Button, Columns, Column } from "bloomer";
 import React, { useState } from "react";
+import ExampleMenu from "../menu";
 import EffectsTree from "../effects";
 import SuperheroTree from "../superhero";
 
-const MyDropdown = ({ clickHandler, labels = [] }) => {
-  const [dropdownStatus, setDropdownStatus] = useState(false);
-  return (
-    <Dropdown
-      style={{ marginBottom: "1.5rem" }}
-      isHidden={["desktop", "tablet"]}
-      isActive={dropdownStatus}
+const ExampleContent = ({ pick }) => (
+  <Column style={{ maxWidth: "calc(100vw - 1.5rem)", overflowX: "scroll" }}>
+    {pick === "superhero" ? <SuperheroTree /> : <EffectsTree />}
+    <Button
+      style={{ marginTop: "1.5rem" }}
+      href={`https://github.com/ldd/react-tech-tree/blob/master/example/src/${pick}/index.js`}
+      target="_blank"
     >
-      <DropdownTrigger>
-        <Button
-          isColor="light"
-          isOutlined
-          aria-haspopup="true"
-          aria-controls="dropdown-menu"
-          onClick={() => setDropdownStatus(!dropdownStatus)}
-        >
-          Example Picker &nbsp; ∨
-        </Button>
-      </DropdownTrigger>
-      <DropdownMenu>
-        <DropdownContent>
-          {labels.map(label => (
-            <DropdownItem key={label} onClick={() => clickHandler(label)}>
-              {label}
-            </DropdownItem>
-          ))}
-        </DropdownContent>
-      </DropdownMenu>
-    </Dropdown>
-  );
-};
-const MenuEntry = ({ clickHandler, label }) => (
-  <li>
-    <MenuLink onClick={() => clickHandler(label)}>{label}</MenuLink>
-  </li>
-);
-const MyMenu = ({ clickHandler, choices = [] }) => (
-  <>
-    <Menu style={{ paddingRight: "1.5rem" }} isHidden="mobile">
-      <MenuLabel>Simple</MenuLabel>
-      <MenuList>
-        <MenuEntry label={choices[0]} clickHandler={clickHandler} />
-      </MenuList>
-      <MenuLabel>Decorated Links</MenuLabel>
-      <MenuList>
-        <MenuEntry label={choices[1]} clickHandler={clickHandler} />
-      </MenuList>
-    </Menu>
-    <MyDropdown labels={choices} clickHandler={clickHandler} />
-  </>
+      Show Code
+    </Button>
+  </Column>
 );
 
 export const Examples = () => {
@@ -70,17 +28,8 @@ export const Examples = () => {
         justifyContent: "space-between"
       }}
     >
-      <MyMenu clickHandler={setPick} choices={choices} />
-      <Column style={{ maxWidth: "calc(100vw - 1.5rem)", overflowX: "scroll" }}>
-        {pick === "superhero" ? <SuperheroTree /> : <EffectsTree />}
-        <Button
-          style={{ marginTop: "1.5rem" }}
-          href={`https://github.com/ldd/react-tech-tree/blob/master/example/src/${pick}/index.js`}
-          target="_blank"
-        >
-          Show Code
-        </Button>
-      </Column>
+      <ExampleMenu clickHandler={setPick} choices={choices} />
+      <ExampleContent pick={pick} />
     </Columns>
   );
 };
