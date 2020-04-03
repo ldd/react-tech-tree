b4d7c36e6a09164dca59dcfd95e91471a5e7d238
ldd <leonardoldd@gmail.com>
2020-04-03 12:18:37 -0400

    Updates

--- example/src/App.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Page } from "./page";
-import "react-tech-tree/dist/index.css";
-
-function App() {
-  return <Page />;
-}
-
-export default App;
---
+++ precache-manifest.23122a6c3da59b29856d0ee84219086e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2909383f3e4e4d5f74908e467109eaa8",
+    "url": "/react-tech-tree/index.html"
+  },
+  {
+    "revision": "7ba1571fa6fe97fd415f",
+    "url": "/react-tech-tree/static/css/2.79ce9370.chunk.css"
+  },
+  {
+    "revision": "079478dd5be085650162",
+    "url": "/react-tech-tree/static/css/main.d6706145.chunk.css"
+  },
+  {
+    "revision": "7ba1571fa6fe97fd415f",
+    "url": "/react-tech-tree/static/js/2.0ba8018a.chunk.js"
+  },
+  {
+    "revision": "a6634ce6e53a17a141e62e64d02190f5",
+    "url": "/react-tech-tree/static/js/2.0ba8018a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "079478dd5be085650162",
+    "url": "/react-tech-tree/static/js/main.8c6cbdca.chunk.js"
+  },
+  {
+    "revision": "9a95b765116112fc7495",
+    "url": "/react-tech-tree/static/js/runtime-main.2d8751d4.js"
+  },
+  {
+    "revision": "1f9729cfca32ef3229b3baa6c02fa068",
+    "url": "/react-tech-tree/static/media/spritesheet.1f9729cf.png"
+  }
+]);
