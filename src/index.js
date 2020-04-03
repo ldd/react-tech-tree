0e1697ce9165f341b13d7a951da3b6c80045edd8
ldd <leonardoldd@gmail.com>
2020-04-03 19:56:30 -0400

    feat: expose Node clickHandler

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 export { Sprite } from "./components/Sprite";
-export { Node } from "./components/Node";
+export { Node, clickHandler as nodeClickHandler } from "./components/Node";
 export { Tree } from "./components/Tree";
 export { prepareSpritesheetStyle, styleScale } from "./helpers/styleHelper";
 export { simplePathMaker } from "./helpers/svgHelper";
