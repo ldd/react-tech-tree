3f3af100e1f307dc60a68282962996a6ae946992
ldd <leonardoldd@gmail.com>
2020-04-07 14:24:25 -0400

    feat(Node): identify children of active Nodes on click

--- src/components/Node.js
+++ src/components/Node.js
@@ -2,14 +2,27 @@ import React from "react";
 import { Sprite } from "./Sprite";
 import "./Node.css";
 
+const toggleChildren = (id = "", isActive) => {
+  // given a link A-N, we are interested in A's child, N
+  const childNode = document.getElementById(id.split("-")[1]);
+  if (!childNode) return;
+
+  if (isActive) childNode.classList.remove("next-active");
+  else childNode.classList.add("next-active");
+};
+
 export const clickHandler = e => {
   // toggle target Node active
+  const isActive = e.target.classList.contains("active");
   e.target.classList.toggle("active");
 
   // toggle links that start with this Node active
   const id = e.target.id;
   const links = document.querySelectorAll(`.Link[id^=${id}]`);
-  links.forEach(link => link.classList.toggle("active"));
+  links.forEach(link => {
+    link.classList.toggle("active");
+    toggleChildren(link.id, isActive);
+  });
 };
 
 export const Node = props => {
