77f17d93b7a4bd4ce227ce9e887170f4986d83d6
ldd <leonardoldd@gmail.com>
2020-04-06 20:10:02 -0400

    test(Link): add Link tests

---
+++ src/components/Link.test.js
@@ -0,0 +1,21 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import { Link } from "./Link";
+
+describe("React Component [Link]", () => {
+  it("can create empty Link", () => {
+    const component = renderer.create(<Link />);
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+  it("can create Link with className", () => {
+    const className = "someClass";
+    const someProp = "@value";
+    const pathData = "M 0 0 L 0 0";
+    const component = renderer.create(
+      <Link className={className} someProp={someProp} pathData={pathData} />
+    );
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
