b5f0478a38f27736ffffa79778979edb87151075
ldd <leonardoldd@gmail.com>
2020-04-07 00:23:14 -0400

    refactor(example): use react-simplest-router

--- example/src/components/page/index.js
+++ example/src/components/page/index.js
@@ -3,9 +3,11 @@ import {
   Hero, HeroBody, HeroHeader, Image, Title,
   Navbar, NavbarBrand, NavbarBurger, NavbarItem, NavbarMenu, NavbarStart
 } from "bloomer";
-import "bulma/css/bulma.min.css";
 import React, { useState } from "react";
-import { useRouter } from "../../hooks/useRouter";
+import useRouter from "react-simplest-router";
+import { Examples } from "./Examples";
+import { Landing } from "./Landing";
+import "bulma/css/bulma.min.css";
 
 const GithubItem = () => (
   <NavbarItem href="https://github.com/ldd/react-tech-tree" target="_blank">
@@ -21,12 +23,12 @@ const GithubItem = () => (
 );
 
 const RoutePicker = ({ routes, clickHandler }) =>
-  routes.map(({ label, route, isActive }) => (
+  routes.map(({ label, location, isActive }) => (
     <NavbarItem
       isActive={isActive}
-      href={route}
-      key={route}
-      onClick={() => clickHandler(route)}
+      href={location}
+      key={location}
+      onClick={() => clickHandler(location)}
     >
       {label}
     </NavbarItem>
@@ -62,8 +64,13 @@ const MyBody = ({ children }) => (
   <HeroBody style={{ padding: "1.5rem" }}>{children}</HeroBody>
 );
 
+const defaultRoutes = [
+  { label: "Home", location: "#/", component: Landing },
+  { label: "Examples", location: "#examples", component: Examples }
+];
+
 export const Page = () => {
-  const { routes, setRoute, ActiveComponent } = useRouter();
+  const { routes, setRoute, ActiveComponent } = useRouter(defaultRoutes);
   return (
     <Hero isColor="info" isSize="medium" isFullHeight isBold>
       <MyHeader routes={routes} setRoute={setRoute} />
